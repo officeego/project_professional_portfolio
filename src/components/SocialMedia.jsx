dd0f9c345b85ee384aa40071aa3efe85ffa2d509
egoheru <egoherokugit@gmail.com>
2022-11-25 17:41:25 +0100

    updated

--- src/components/SocialMedia.jsx
+++ src/components/SocialMedia.jsx
@@ -1,9 +1,20 @@
 import React from 'react';
 import { BsTwitter, BsInstagram } from 'react-icons/bs';
 import { FaFacebookF } from 'react-icons/fa';
+// import { Link } from 'react-router-dom';
 
 const SocialMedia = () => (
   <div className="app__social">
+    {/* <div>
+      <Link to="/www.facebook.com/tazzercleaners" target="_blank">
+        <BsTwitter />
+      </Link>
+    </div> */}
+    <div>
+      <a href="/https://instagram.com" target="_blank">
+        <BsTwitter />
+      </a>
+    </div>
     <div>
       <BsTwitter />
     </div>
